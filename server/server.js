4c0b284ecd495948070c424d275b278a2424d09e
Arnab Senapati <senapatiarnab676@gmail.com>
2025-07-21 20:38:14 +0530

    Update allowed origins for CORS configuration

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ await connectCloudinary();
 // Allow multiple frontend origins
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://greencart-gold.vercel.app'  // Replace with your actual deployed frontend URL
+  'https://greencart-ten-kohl.vercel.app'  // Replace with your actual deployed frontend URL
 ];
 
 // Stripe raw route must come before express.json()
